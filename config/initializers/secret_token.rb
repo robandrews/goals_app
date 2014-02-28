757e4672aa373961cb43ea584240ae064a0f9365
App Academy Student <student@appacademy.io>
2014-02-28 10:30:08 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoalApp::Application.config.secret_key_base = 'e8e4cd64c7e489e9856081ce2e38896e927ad0ada8a106985c64959150c5f1cef0c28a7ca84faa7cd7273e0ac4f0291a302bd75d6e2ec4918c2b02f6df201221'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GoalApp::Application.config.session_store :cookie_store, key: '_GoalApp_session'
